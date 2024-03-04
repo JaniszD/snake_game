27ebda53abaf375ecdd7b672558226f61d5fa659
JaniszD <janekdardzinski@gmail.com>
2024-03-05 00:26:00 +0100

    dodajemy pierwszy plik .eslintrc.cjs

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import WebApp from '@twa-dev/sdk'
+
+WebApp.ready();
+
+ReactDOM.createRoot...
+
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
