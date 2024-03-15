4ed45c9a646a12f757928d1e869fa8739f62bab1
JaniszD <janekdardzinski@gmail.com>
2024-03-15 18:24:25 +0100

    commit1

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import WebApp from '@twa-dev/sdk'
-
-WebApp.ready();
-
-ReactDOM.createRoot...
-
-
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import WebApp from '@twa-dev/sdk'
+
+WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
